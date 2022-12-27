f5a48d6ad0e058ec3b5809f6da9408ee83107bfc
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-27 12:18:00 +0100

    Add function to convert date

--- ex-social-posts-starter/js/script.js
+++ ex-social-posts-starter/js/script.js
@@ -77,3 +77,21 @@ const posts = [
     
 
 ]
+
+console.log(posts);
+
+//Prendo gli elementi dal DOM
+const targetPostList = document.getElementById('container');
+
+
+//# FUNZIONI
+
+//Funzione per convertire data
+const convertDate = (date) => {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    return `${day}/${month}/${year}`
+}
